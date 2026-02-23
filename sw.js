0ab0a611de35a13cdff6e15610701063feb0e58e
nicolaattardi-tech <nicola.attardi@gmail.com>
2026-02-23 15:49:04 +0800

    V1.01.001
    Very basic water intake app, no records and no custom features

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'hydrotrack-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// 1. Install Event - Caches the files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Opened cache');
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// 2. Fetch Event - Serves cached files if offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // Return the cached version if found, otherwise fetch from the network
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// 3. Activate Event - Cleans up old caches if you update CACHE_NAME
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
